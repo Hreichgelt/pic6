ae8fae84d539b3d1becffadff2dab6967bb8a7eb
Hans Reichgelt <hreichgelt06@gmail.com>
2022-09-17 21:10:12 -0500

    snagged

--- pic6/src/App.jsx
+++ pic6/src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getImages } from './api';
 import './App.css'
-import images from "./api-mock.json"
 
 const App = () => {
-  const [imageList, setImageList] = useState(images.resources);
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const responseJson = await getImages();
+      setImageList(responseJson.resources)
+    }
+
+    fetchData();
+  }, []);
+
   return <div className='image-grid'>
     {imageList.map((image)=> <img src={image.url} alt={image.public_id}></img>)}
   </div>;
