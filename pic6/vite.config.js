1c62ba6b4d10d3cdc1e7acb304da31c421b957b4
Hans Reichgelt <hreichgelt06@gmail.com>
2022-09-18 22:59:36 -0500

    just adding notes for the same issue I have been having

--- pic6/vite.config.js
+++ pic6/vite.config.js
@@ -5,3 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
+
+// There seems to be some kind of issue with configuration here and I cannot find anything on Vite docs or anyting newer in forums. Seems 
+// that everything is from at least a year ago. Going to need support with this one. 
+
+// Env Variables#
+// Vite exposes env variables on the special import.meta.env object. Some built-in variables are available in all cases:
+
+// import.meta.env.MODE: {string} the mode the app is running in.
+
+// import.meta.env.BASE_URL: {string} the base url the app is being served from. This is determined by the base config option.
+
+// import.meta.env.PROD: {boolean} whether the app is running in production.
+
+// import.meta.env.DEV: {boolean} whether the app is running in development (always the opposite of import.meta.env.PROD)
+
+// import.meta.env.SSR: {boolean} whether the app is running in the server.
